75e83fcaa128d9c89f93c951df16b4752f70439d
Vladimír Polak <vladimir.polak.ml@gmail.com>
2025-02-19 22:04:42 +0100

    fix: Update integer type hints to int in UserDTO and composer.json

--- output_dir/UserDTO.php
+++ output_dir/UserDTO.php
@@ -6,18 +6,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 class UserDTO
 {
-    private readonly integer $id;
+    private readonly int $id;
     private readonly string $name;
     private readonly string $email;
 
-    public function __construct(integer $id, string $name, string $email, )
+    public function __construct(int $id, string $name, string $email, )
     {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
     }
 
-    public function get(): integer
+    public function get(): int
     {
         return $this->id;
     }
@@ -34,46 +34,6 @@ class UserDTO
     {
         return new self(
             $request->get('id'), $request->get('name'), $request->get('email'), 
-        );
-    }
-}
-
-use Symfony\Component\HttpFoundation\Request;
-
-class UserDTO
-{
-    private readonly int $id;
-    private readonly string $name;
-    private readonly string $email;
-
-    public function __construct(int $id, string $name, string $email)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
-    }
-
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public static function fromRequest(Request $request): self
-    {
-        return new self(
-            $request->get('id'),
-            $request->get('name'),
-            $request->get('email')
         );
     }
 }
